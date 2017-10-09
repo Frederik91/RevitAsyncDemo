27ce48282236bd194c2f74899956a7cd8b4355fb
Frederik91 <frederik.t91@gmail.com>
2017-10-09 23:01:49 +0200

    Add files via upload

---
+++ RevitInteractors/CompositionRoot.cs
@@ -0,0 +1,18 @@
+﻿using Contracts.RevitInteractors;
+using LightInject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitInteractors
+{
+    public class CompositionRoot : ICompositionRoot
+    {
+        public void Compose(IServiceRegistry serviceRegistry)
+        {
+            serviceRegistry.Register<IDocumentRevitInteractor, DocumentRevitInteractor>(new PerScopeLifetime());
+        }
+    }
+}
